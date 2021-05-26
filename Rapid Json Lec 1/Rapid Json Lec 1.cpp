b5458fb36d08eba7e1bf788e73633f9f59378a4e
michael sipayung <michaelsipayung123@gmail.com>
2021-05-27 06:47:45 +0700

    testing, case : cmake configuration

--- Rapid Json Lec 1/Rapid Json Lec 1.cpp
+++ Rapid Json Lec 1/Rapid Json Lec 1.cpp
@@ -5,41 +5,38 @@
 
 
 int main()
-{
-	/*
-	std::cout << "Hello CMake." << std::endl;
-	//parse a json into dom 
+{	
+	// 1. parse json string into dom 
 	const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
 	rapidjson::Document d;
 	d.Parse(json);
 
-	//modify it  by dom 
-	rapidjson::Value& s = d["stars"];
-	s.SetInt(s.GetInt() + 1);
-
-	//stringfy the dom 
-	rapidjson::StringBuffer buffer;
-	rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
-	d.Accept(buffer);
-
-	//set output 
-	std::cout << buffer.GetString() << std::endl;
-		*/		
-	const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
-	rapidjson::Document d;
-	d.Parse(json);
-
+	//2.modify it  by dom 
 	rapidjson::Value& s = d["stars"];
 	rapidjson::Value& str = d["project"];
-
 	str.SetString("rapidjson++");
 
+	//3.stringfy the dom 
 	rapidjson::StringBuffer buffer;
 	rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
 	d.Accept(writer);
 
 	std::cout << "hello rapid json . a ligeweight c++ for json parser" << std::endl;
 	std::cout << buffer.GetString()<< std::endl;
+	
+	std::cout << "Set cmake configuration" << std::endl;
+	const char* configuration = "{\"name\": \"x64 - Debug\",\"generator\": \"Ninja\",\"configurationType\": \"Debug\"}";
+	rapidjson::Document test;
+	test.Parse(configuration);
+	rapidjson::StringBuffer buf;
+	rapidjson::Writer<rapidjson::StringBuffer> writerTest(buf);
+	test.Accept(writerTest);
+	std::cout << " test reading cmake configuration.json" << std::endl;
+	std::cout << buf.GetString() << std::endl;
+
+
+
+
 
 	return 0;
 }

308df2117aa248768063ee4c25ed07ac2916c167
michael sipayung <michaelsipayung123@gmail.com>
2021-05-27 06:35:17 +0700

    Add project files.

---
+++ Rapid Json Lec 1/Rapid Json Lec 1.cpp
@@ -0,0 +1,45 @@
+﻿// Rapid Json Lec 1.cpp : Defines the entry point for the application.
+//
+
+#include "Rapid Json Lec 1.h"
+
+
+int main()
+{
+	/*
+	std::cout << "Hello CMake." << std::endl;
+	//parse a json into dom 
+	const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+	rapidjson::Document d;
+	d.Parse(json);
+
+	//modify it  by dom 
+	rapidjson::Value& s = d["stars"];
+	s.SetInt(s.GetInt() + 1);
+
+	//stringfy the dom 
+	rapidjson::StringBuffer buffer;
+	rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+	d.Accept(buffer);
+
+	//set output 
+	std::cout << buffer.GetString() << std::endl;
+		*/		
+	const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+	rapidjson::Document d;
+	d.Parse(json);
+
+	rapidjson::Value& s = d["stars"];
+	rapidjson::Value& str = d["project"];
+
+	str.SetString("rapidjson++");
+
+	rapidjson::StringBuffer buffer;
+	rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+	d.Accept(writer);
+
+	std::cout << "hello rapid json . a ligeweight c++ for json parser" << std::endl;
+	std::cout << buffer.GetString()<< std::endl;
+
+	return 0;
+}
---
+++ Rapid Json Lec 1/Rapid Json Lec 1.h
@@ -0,0 +1,11 @@
+﻿// Rapid Json Lec 1.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <rapidjson/document.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
+
+// TODO: Reference additional headers your program requires here.

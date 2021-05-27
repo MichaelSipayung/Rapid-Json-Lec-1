5890f2d1ef5ee75461e6e71a59a359b1fddac2ad
michael sipayung <michaelsipayung123@gmail.com>
2021-05-27 12:34:12 +0700

    access member json

--- Rapid Json Lec 1/Rapid Json Lec 1.cpp
+++ Rapid Json Lec 1/Rapid Json Lec 1.cpp
@@ -34,8 +34,86 @@ int main()
 	std::cout << " test reading cmake configuration.json , test" << std::endl;
 	std::cout << buf.GetString() << std::endl;
 
+	std::cout << "Lecture 1" << std::endl;
+	const char* jsonVar= "{\"hello\": \"world\",\"t\" : true ,\"f\" : false,\"n\" : null,\"i\" : 123,\"pi\" : 3.1416,\"a\" : [1, 2, 3, 4]}";
 
+	//parse it into document
+	rapidjson::Document  doc;
+	doc.Parse(jsonVar);
+	assert(doc.HasMember("hello")); //test if it's has a  member hello
+	assert(doc["t"].IsBool());//assertion success
+	std::cout << "Show string \t: [" << doc["hello"].GetString() << "]" << std::endl;
+	std::cout << "Show pi value \t: [" << doc["pi"].GetDouble() << "]" << std::endl;
+	std::cout << "Show boolean value \t: [" << doc["t"].GetBool() << "]" << std::endl;
+	if (doc["t"].GetBool())
+	{
+		std::cout << "True" << std::endl;
+	}
+	else {
+		std::cout << "False" << std::endl;
+	}
 
+	std::cout << "Test if it's null" << std::endl;
+	if (doc["n"].IsNull())
+	{
+		std::cout << "it contain  null value" << std::endl;
+	}
+	else {
+		std::cout << "not null, other type" << std::endl;
+	}
+
+	//test assertion for type int 
+
+	assert(doc["i"].IsNumber()); //raise an exception if not
+	assert(doc["i"].IsInt());
+	std::cout << "test suceess" << std::endl;
+
+	std::cout << "Json array" << std::endl;
+	const rapidjson::Value& val = doc["a"];
+	//testing assertion 
+	std::cout << "show array\t: [";
+	for (auto i = 0; i != val.Size(); ++i) {
+		std::cout << i << " -> " << val[i].GetInt() << "|";
+	}
+	std::cout << "]" << std::endl;
+	
+	std::cout << "query array " << std::endl;
+	std::cout << "Traverse element using iterator \t\t: [";
+	for (rapidjson::Value::ConstValueIterator itr = val.Begin(); itr != val.End(); ++itr) {
+		std::cout << itr->GetInt() << "|";
+	}
+	std::cout << "]" << std::endl;
+
+	std::cout << "Using modern c++ to traverse the element\t:[";
+	for (auto& i : val.GetArray()) {
+		std::cout << i.GetInt() << "|";
+	}
+	std::cout << "]" << std::endl;
+	std::cout << "Traverse all array json range based for \t:[ ";
+	for (auto& i : doc["a"].GetArray()) {
+		std::cout << i.GetInt() << "|";
+	}
+	std::cout << "]" << std::endl;
+
+	std::cout << "Query Object" << std::endl;
+	static const char* kTypeNames[]={ "Null", "False", "True", "Object", "Array", "String", "Number" };
+	for (rapidjson::Value::ConstMemberIterator i = doc.MemberBegin(); i != doc.MemberEnd(); ++i) {
+		std::cout << i->name.GetString()<<" | "<< kTypeNames[i->value.GetType()] << std::endl;
+	}
+	std::cout << "Ensure whether a member exist\t:[";
+	rapidjson::Value::MemberIterator itr1 = doc.FindMember("notExist");
+	if (itr1!=doc.MemberEnd()) //traverse the element
+	{
+		std::cout << "exist \t: [ " << itr1->value.GetString() << "]" << std::endl;
+	}
+	else {
+		std::cout << "member  not exist" << std::endl;
+	}
+
+	std::cout << "Range based for loop to traverse all object" << std::endl;
+	for (auto& i : doc.GetObject()) {
+		std::cout << "Object \t:[ " << i.name.GetString() << "|" << kTypeNames[i.value.GetType()] <<std::endl;
+	}
 
 
 	return 0;
